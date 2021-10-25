df1ec22e2566cd5b94616c32e13930e59780b3b5
Fabian Zbrański <fabian.zbranski@gmail.com>
2021-10-25 19:00:00 +0200

    move mechanism from previous commit to functions

--- src/Main.java
+++ src/Main.java
@@ -13,115 +13,126 @@ public class Main {
         Player player = new Player("Player 2", 'O');
         while(doWeHaveWinner(checkBoard)){
             printCheckBoard(checkBoard);
-            if(!error)
-                if(player.name == "Player 2"){
-                    player.name = "Player 1";
-                    player.OorX = 'X';
-                }
-                else {
-                    player.name = "Player 2";
-                    player.OorX = 'O';
-                }
+            didPlayerMakeMistake(error, player);
             System.out.println(player.name + " where do you want to place your mark?");
             place = scanner.nextByte();
-            switch (place){
-                case 1:
-                    if(checkBoard[0][0] == ' ') {
-                        checkBoard[0][0] = player.OorX;
-                        error = false;
-                    }
-                    else {
-                        System.out.println("This place is already taken try again");
-                        error = true;
-                    }
-                    break;
-                case 2:
-                    if(checkBoard[0][2] == ' ') {
-                        checkBoard[0][2] = player.OorX;
-                        error = false;
-                    }
-                    else {
-                        System.out.println("This place is already taken try again");
-                        error = true;
-                    }
-                    break;
-                case 3:
-                    if(checkBoard[0][4] == ' ') {
-                        checkBoard[0][4] = player.OorX;
-                        error = false;
-                    }
-                    else {
-                        System.out.println("This place is already taken try again");
-                        error = true;
-                    }
-                    break;
-                case 4:
-                    if(checkBoard[2][0] == ' ') {
-                        checkBoard[2][0] = player.OorX;
-                        error = false;
-                    }
-                    else {
-                        System.out.println("This place is already taken try again");
-                        error = true;
-                    }
-                    break;
-                case 5:
-                    if(checkBoard[2][2] == ' ') {
-                        checkBoard[2][2] = player.OorX;
-                        error = false;
-                    }
-                    else {
-                        System.out.println("This place is already taken try again");
-                        error = true;
-                    }
-                    break;
-                case 6:
-                    if(checkBoard[2][4] == ' ') {
-                        checkBoard[2][4] = player.OorX;
-                        error = false;
-                    }
-                    else {
-                        System.out.println("This place is already taken try again");
-                        error = true;
-                    }
-                    break;
-                case 7:
-                    if(checkBoard[4][0] == ' ') {
-                        checkBoard[4][0] = player.OorX;
-                        error = false;
-                    }
-                    else {
-                        System.out.println("This place is already taken try again");
-                        error = true;
-                    }
-                    break;
-                case 8:
-                    if(checkBoard[4][2] == ' ') {
-                        checkBoard[4][2] = player.OorX;
-                        error = false;
-                    }
-                    else {
-                        System.out.println("This place is already taken try again");
-                        error = true;
-                    }
-                    break;
-                case 9:
-                    if(checkBoard[4][4] == ' ') {
-                        checkBoard[4][4] = player.OorX;
-                        error = false;
-                    }
-                    else {
-                        System.out.println("This place is already taken try again");
-                        error = true;
-                    }
-                    break;
-                default:
-                    System.out.println("You can choose number only beetwen 1-9");
-                    break;
-            }
+            error = placingMark(place, player, checkBoard, error);
+
         }
         System.out.println("Congrats " + player.name + " you are the winner !!!");
     }
+// ----------------------------------------------------------------------------------------------------------
+    static void didPlayerMakeMistake(boolean error, Player player){
+        if(!error)
+            if(player.name == "Player 2"){
+                player.name = "Player 1";
+                player.OorX = 'X';
+            }
+            else {
+                player.name = "Player 2";
+                player.OorX = 'O';
+            }
+    }
+// ----------------------------------------------------------------------------------------------------------
+    static boolean placingMark(byte place, Player player, char[][] checkBoard, boolean error){
+        switch (place){
+            case 1:
+                if(checkBoard[0][0] == ' ') {
+                    checkBoard[0][0] = player.OorX;
+                    error = false;
+                }
+                else {
+                    System.out.println("This place is already taken try again");
+                    error = true;
+                }
+                break;
+            case 2:
+                if(checkBoard[0][2] == ' ') {
+                    checkBoard[0][2] = player.OorX;
+                    error = false;
+                }
+                else {
+                    System.out.println("This place is already taken try again");
+                    error = true;
+                }
+                break;
+            case 3:
+                if(checkBoard[0][4] == ' ') {
+                    checkBoard[0][4] = player.OorX;
+                    error = false;
+                }
+                else {
+                    System.out.println("This place is already taken try again");
+                    error = true;
+                }
+                break;
+            case 4:
+                if(checkBoard[2][0] == ' ') {
+                    checkBoard[2][0] = player.OorX;
+                    error = false;
+                }
+                else {
+                    System.out.println("This place is already taken try again");
+                    error = true;
+                }
+                break;
+            case 5:
+                if(checkBoard[2][2] == ' ') {
+                    checkBoard[2][2] = player.OorX;
+                    error = false;
+                }
+                else {
+                    System.out.println("This place is already taken try again");
+                    error = true;
+                }
+                break;
+            case 6:
+                if(checkBoard[2][4] == ' ') {
+                    checkBoard[2][4] = player.OorX;
+                    error = false;
+                }
+                else {
+                    System.out.println("This place is already taken try again");
+                    error = true;
+                }
+                break;
+            case 7:
+                if(checkBoard[4][0] == ' ') {
+                    checkBoard[4][0] = player.OorX;
+                    error = false;
+                }
+                else {
+                    System.out.println("This place is already taken try again");
+                    error = true;
+                }
+                break;
+            case 8:
+                if(checkBoard[4][2] == ' ') {
+                    checkBoard[4][2] = player.OorX;
+                    error = false;
+                }
+                else {
+                    System.out.println("This place is already taken try again");
+                    error = true;
+                }
+                break;
+            case 9:
+                if(checkBoard[4][4] == ' ') {
+                    checkBoard[4][4] = player.OorX;
+                    error = false;
+                }
+                else {
+                    System.out.println("This place is already taken try again");
+                    error = true;
+                }
+                break;
+            default:
+                System.out.println("You can choose number only beetwen 1-9");
+                break;
+        }
+        return error;
+    }
+// ----------------------------------------------------------------------------------------------------------
     static void printCheckBoard(char[][] checkBoard){
         for(int i=0; i<5; i++){
             for(int j=0; j<5; j++){
@@ -130,6 +141,7 @@ public class Main {
             System.out.println();
         }
     }
+// ----------------------------------------------------------------------------------------------------------
     static boolean doWeHaveWinner(char[][] checkboard){
         if( (checkboard[0][0] == 'X' && checkboard[0][2] == 'X' && checkboard [0][4] == 'X') ||
             (checkboard[2][0] == 'X' && checkboard[2][2] == 'X' && checkboard [2][4] == 'X') ||
