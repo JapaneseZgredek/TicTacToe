af6687ea32a26ff1bf33964a8acf75854ac04396
Fabian Zbrański <fabian.zbranski@gmail.com>
2021-10-25 18:29:56 +0200

    add function called doWeHaveWinner

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,9 @@ public class Main {
                                 {'-','+','-','+','-'},
                                 {' ','|',' ','|',' '}};
         printCheckBoard(checkBoard);
+        while(doWeHaveWinner(checkBoard)){
+            
+        }
     }
     static void printCheckBoard(char[][] checkBoard){
         for(int i=0; i<5; i++){
@@ -14,5 +17,27 @@ public class Main {
             }
             System.out.println();
         }
+    }
+    static boolean doWeHaveWinner(char[][] checkboard){
+        if( (checkboard[0][0] == 'X' && checkboard[0][2] == 'X' && checkboard [0][4] == 'X') ||
+            (checkboard[2][0] == 'X' && checkboard[2][2] == 'X' && checkboard [2][4] == 'X') ||
+            (checkboard[4][0] == 'X' && checkboard[4][2] == 'X' && checkboard [4][4] == 'X') ||
+            (checkboard[0][0] == 'X' && checkboard[2][0] == 'X' && checkboard [4][0] == 'X') ||
+            (checkboard[0][2] == 'X' && checkboard[2][2] == 'X' && checkboard [4][2] == 'X') ||
+            (checkboard[0][4] == 'X' && checkboard[2][4] == 'X' && checkboard [4][4] == 'X') ||
+            (checkboard[0][0] == 'X' && checkboard[2][2] == 'X' && checkboard [4][4] == 'X') ||
+            (checkboard[0][4] == 'X' && checkboard[2][2] == 'X' && checkboard [4][0] == 'X'))
+            return false;
+        else if( (checkboard[0][0] == 'O' && checkboard[0][2] == 'O' && checkboard [0][4] == 'O') ||
+                (checkboard[2][0] == 'O' && checkboard[2][2] == 'O' && checkboard [2][4] == 'O') ||
+                (checkboard[4][0] == 'O' && checkboard[4][2] == 'O' && checkboard [4][4] == 'O') ||
+                (checkboard[0][0] == 'O' && checkboard[2][0] == 'O' && checkboard [4][0] == 'O') ||
+                (checkboard[0][2] == 'O' && checkboard[2][2] == 'O' && checkboard [4][2] == 'O') ||
+                (checkboard[0][4] == 'O' && checkboard[2][4] == 'O' && checkboard [4][4] == 'O') ||
+                (checkboard[0][0] == 'O' && checkboard[2][2] == 'O' && checkboard [4][4] == 'O') ||
+                (checkboard[0][4] == 'O' && checkboard[2][2] == 'O' && checkboard [4][0] == 'O'))
+            return false;
+        else
+            return true;
     }
 }
